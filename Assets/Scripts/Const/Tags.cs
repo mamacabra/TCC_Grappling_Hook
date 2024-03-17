9dd3a6b63c2dcb6d29b125e6adef9e2913f2a8d1
Jonathan Bergson <contato@bergson.me>
2024-03-17 17:26:03 -0300

    chore(tags): Criado arquivos de constantes para comprar Tags de GameObjects

---
+++ Assets/Scripts/Const/Tags.cs
@@ -0,0 +1,9 @@
+namespace Const
+{
+    public class Tags
+    {
+        public const string Character = "Character";
+        public const string Object = "Object";
+        public const string Wall = "Wall";
+    }
+}
