8780715f4d5beed8fd832bc789220a41bd4a7b16
Jonathan Bergson <contato@bergson.me>
2024-03-18 00:16:16 -0300

    chore: CharacterUI.cs olhando para camera

--- Assets/Scripts/Character/CharacterUI.cs
+++ Assets/Scripts/Character/CharacterUI.cs
@@ -1,21 +1,31 @@
+using System;
 using Character.Utils;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Character
 {
+    [ExecuteInEditMode]
     public class CharacterUI : ACharacterMonoBehaviour
     {
+        [SerializeField] private GameObject characterUI;
+
         [Header("Character UI")]
-        public Text statusText;
+        [SerializeField] private Text statusText;
 
         [Header("Grappling Hook UI")]
-        public Text forceText;
+        [SerializeField] private Text forceText;
 
         private void Start()
         {
+            characterUI = GameObject.Find("CharacterUI");
             statusText = GameObject.Find("Character - StateText").GetComponent<Text>();
             forceText = GameObject.Find("GrapplingHook - ForceText").GetComponent<Text>();
+        }
+
+        private void Update()
+        {
+            characterUI?.transform.LookAt(Camera.main.transform.position);
         }
 
         public void UpdateStatusUI(string status)
