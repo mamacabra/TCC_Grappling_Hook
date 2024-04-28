32b05f0bbe56a8bd9639a2d6aeee3edb1e28ac77
Jonathan Bergson <contato@bergson.me>
2024-04-28 17:16:45 -0300

    refactor(WalkState): atualiza lógica de movimento e animação
    O código de movimentação do personagem foi simplificado e refatorado. A velocidade de caminhada foi ligeiramente aumentada e a velocidade de animação do passo foi adicionada. A orientação do personagem agora é atualizada no FixedUpdate em vez de no Update.

--- Assets/Scripts/Character/States/WalkState.cs
+++ Assets/Scripts/Character/States/WalkState.cs
@@ -5,7 +5,8 @@ namespace Character.States
 {
     public class WalkState : ACharacterState
     {
-        private const float WalkSpeed = 15f;
+        private const float WalkSpeed = 16f;
+        private const float WalkAnimationSpeed = 3f;
         private const float RotationSpeed = 1000f;
 
         private float movementMagnitude;
@@ -23,22 +24,12 @@ namespace Character.States
 
         public override void Update()
         {
-            var movementInput = CharacterEntity.CharacterInput.movementInput;
-            var direction = new Vector3(movementInput.x, 0, movementInput.y);
+            var axes = CharacterEntity.CharacterInput.movementInput;
+            var direction = new Vector3(axes.x, 0, axes.y);
 
             if (_hasHit == false)
             {
                 CharacterEntity.Rigidbody.MovePosition(CharacterEntity.Rigidbody.transform.position + direction * (WalkSpeed * Time.deltaTime));
-            }
-
-            if (direction != Vector3.zero)
-            {
-                var transform = CharacterEntity.CharacterInput.transform;
-                var toRotation = Quaternion.LookRotation(direction);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, RotationSpeed * Time.deltaTime);
-            }
-            else{
-                CharacterEntity.Rigidbody.Sleep();
             }
 
             movementMagnitude = direction.magnitude;
@@ -50,6 +41,10 @@ namespace Character.States
 
         public override void FixedUpdate()
         {
+            var axes = CharacterEntity.CharacterInput.movementInput;
+            var direction = new Vector3(axes.x, 0, axes.y) + Transform.position;
+            Transform.LookAt(direction);
+
             RaycastTest();
         }
 
