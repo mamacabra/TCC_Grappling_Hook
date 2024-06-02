60bc7d097d134ceef2be7f107f8ba1be71f9698f
Jonathan Bergson <contato@bergson.me>
2024-06-02 15:45:59 -0300

    refactor(Character): Mover funcionalidades de raycast de WalkState para ACharacterState
    A funcionalidade de teste de raycast na classe WalkState foi removida e implementada na classe ACharacterState, com ampliações para testar colisões à esquerda, ao centro e à direita do personagem. A lógica de movimento também foi atualizada para ajustar a direção com base nos resultados do teste de raycast. Desta forma, a estrutura de controle de estado foi simplificada, aprimorando a divisão de responsabilidades entre as classes.

--- Assets/Scripts/Character/States/WalkState.cs
+++ Assets/Scripts/Character/States/WalkState.cs
@@ -1,14 +1,9 @@
 using Character.Utils;
-using UnityEngine;
 
 namespace Character.States
 {
     public class WalkState : ACharacterState
     {
-        private bool hasHit;
-        private const float RaycastDistance = 1f;
-        private Color RaycastColor => hasHit ? Color.red : Color.green;
-
         public WalkState(CharacterEntity characterEntity) : base(characterEntity) {}
 
         public override void Enter()
@@ -16,32 +11,10 @@ namespace Character.States
             CharacterEntity.GrapplingHookWeapon.ResetHook();
         }
 
-        public override void Update()
+        public override void FixedUpdate()
         {
-            RaycastTest();
-            if (hasHit == false) Walk();
+            Walk();
             LookAt();
-        }
-
-        private void RaycastTest()
-        {
-            var axes = CharacterEntity.CharacterInput.Axes;
-            var moveDirection = new Vector3(axes.x, 0, axes.y);
-            var position = Transform.position;
-            var direction = moveDirection;
-            var origin = new Vector3(position.x, 1f, position.z) + direction;
-
-            Physics.Raycast(origin, direction, out var hit, RaycastDistance);
-            Debug.DrawRay(origin, direction * RaycastDistance, RaycastColor);
-
-            if (hit.collider)
-            {
-                hasHit = hit.collider.CompareTag(Const.Tags.Wall) || hit.collider.CompareTag(Const.Tags.Object);
-            }
-            else
-            {
-                hasHit = false;
-            }
         }
     }
 }
--- Assets/Scripts/Character/Utils/ACharacterState.cs
+++ Assets/Scripts/Character/Utils/ACharacterState.cs
@@ -9,6 +9,14 @@ namespace Character.Utils
 
         private const float WalkSpeed = 16f;
 
+        private bool hasHitLeft;
+        private Color RaycastColorLeft => hasHitLeft ? Color.red : Color.green;
+        private bool hasHitCenter;
+        private Color RaycastColorCenter => hasHitCenter ? Color.red : Color.green;
+        private bool hasHitRight;
+        private Color RaycastColorRight => hasHitRight ? Color.red : Color.green;
+        private const float RaycastDistance = 2f;
+
         protected ACharacterState(CharacterEntity characterEntity)
         {
             CharacterEntity = characterEntity;
@@ -22,24 +30,61 @@ namespace Character.Utils
 
         protected void Walk(float speed = WalkSpeed)
         {
-            var direction = CharacterEntity.CharacterInput.MoveDirection;
-            Transform.Translate(direction * (speed * Time.deltaTime));
-
             if (CharacterEntity.CharacterMesh.animator)
             {
                 var magnitude = CharacterEntity.CharacterInput.MoveDirection.magnitude;
                 CharacterEntity.CharacterMesh.animator.SetFloat("Speed", magnitude);
             }
+
+            var direction = Vector3.forward * CharacterEntity.CharacterInput.MoveDirection.magnitude;
+            var origin = new Vector3(Transform.position.x, 1f, Transform.position.z);
+
+            var rayLeftDirection = (Transform.forward + Transform.right * -1).normalized;
+            Physics.Raycast(origin, rayLeftDirection, out var hitLeft, RaycastDistance);
+            Debug.DrawRay(origin, rayLeftDirection * RaycastDistance, RaycastColorLeft);
+
+            var rayCenterDirection = Transform.forward;
+            Physics.Raycast(origin, rayCenterDirection, out var hitCenter, RaycastDistance);
+            Debug.DrawRay(origin, rayCenterDirection * RaycastDistance, RaycastColorCenter);
+
+            var rayRightDirection = (Transform.forward + Transform.right).normalized;
+            Physics.Raycast(origin, rayRightDirection, out var hitRight, RaycastDistance);
+            Debug.DrawRay(origin, rayRightDirection * RaycastDistance, RaycastColorRight);
+
+            if (hitLeft.collider && hitCenter.collider && hitRight.collider) return;
+
+            if (hitLeft.collider)
+            {
+                hasHitLeft = hitLeft.collider.CompareTag(Const.Tags.Wall) || hitLeft.collider.CompareTag(Const.Tags.Object);
+                direction += Vector3.right * 0.5f;
+            }
+            else hasHitLeft = false;
+
+            if (hitCenter.collider)
+            {
+                hasHitCenter = hitCenter.collider.CompareTag(Const.Tags.Wall) || hitCenter.collider.CompareTag(Const.Tags.Object);
+                direction = new Vector3(direction.x, direction.y, 0);
+            }
+            else hasHitCenter = false;
+
+            if (hitRight.collider)
+            {
+                hasHitRight = hitRight.collider.CompareTag(Const.Tags.Wall) || hitRight.collider.CompareTag(Const.Tags.Object);
+                direction += Vector3.right * -0.5f;
+            }
+            else hasHitRight = false;
+
+            direction = direction.normalized;
+            Transform.Translate(direction * (speed * Time.deltaTime));
         }
 
         protected void LookAt()
         {
             var direction = CharacterEntity.CharacterInput.MoveDirection;
-            if (direction != Vector3.zero)
-            {
-                var lookDirection = CharacterEntity.CharacterInput.LookDirection;
-                CharacterEntity.Character.characterBody.LookAt(lookDirection);
-            }
+            if (direction == Vector3.zero) return;
+
+            var lookDirection = CharacterEntity.CharacterInput.LookDirection;
+            CharacterEntity.Character.characterBody.LookAt(lookDirection);
         }
     }
 }
