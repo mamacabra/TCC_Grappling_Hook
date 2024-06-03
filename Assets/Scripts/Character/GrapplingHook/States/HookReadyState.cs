3a18af3fd1f2d19856113ee80adfae02acb48131
Jonathan Bergson <contato@bergson.me>
2024-06-02 21:58:30 -0300

    feat(GrapplingHook): adiciona método Enter em HookReadyState
    O método Enter foi adicionado à classe HookReadyState. Este método desativa o GrapplingHookCollider e redefine a posição local do GrapplingHookTransform para a posição de origem. Essa mudança auxilia na inicialização adequada do estado do gancho sempre que o personagem entra neste estado.

--- Assets/Scripts/Character/GrapplingHook/States/HookReadyState.cs
+++ Assets/Scripts/Character/GrapplingHook/States/HookReadyState.cs
@@ -5,5 +5,11 @@ namespace Character.GrapplingHook.States
     public class HookReadyState : AGrapplingHookState
     {
         public HookReadyState(CharacterEntity characterEntity) : base(characterEntity) { }
+
+        public override void Enter()
+        {
+            CharacterEntity.GrapplingHookCollider.enabled = false;
+            CharacterEntity.GrapplingHookTransform.localPosition = GrapplingStats.originPosition;
+        }
     }
 }
