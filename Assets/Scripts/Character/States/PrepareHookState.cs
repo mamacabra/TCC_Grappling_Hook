2457f72192a8daf6ac53b1b4a45e8e8a6591ff58
Jonathan Bergson <contato@bergson.me>
2024-06-02 23:11:37 -0300

    refactor(Character): Ajusta tempo de preparação do gancho
    O valor de CountDownStep em PrepareHookState foi alterada de 0.1f para 0.2f para aumentar o tempo de preparação do gancho.

--- Assets/Scripts/Character/States/PrepareHookState.cs
+++ Assets/Scripts/Character/States/PrepareHookState.cs
@@ -6,7 +6,7 @@ namespace Character.States
     public class PrepareHookState : ACharacterState
     {
         private float countDown;
-        private const float CountDownStep = 0.1f;
+        private const float CountDownStep = 0.2f;
         private const float WalkSpeed = 8f;
 
         public PrepareHookState(CharacterEntity characterEntity) : base(characterEntity) {}
