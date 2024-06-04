684c603f7cc5ed92164e7a51b0eb5f811ba2e58c
Jonathan Bergson <contato@bergson.me>
2024-06-04 12:01:14 -0300

    feat(Character States): adiciona verificação de colisão em DispatchHookState
    Nesse commit, um Raycast foi adicionado ao método Enter de DispatchHookState para verificar a colisão antes de mudar o estado do personagem. Agora, se houver colisão, o estado de caminhada é configurado, caso contrário, o estado de gancho de agarre é ativado.

--- Assets/Scripts/Character/States/DispatchHookState.cs
+++ Assets/Scripts/Character/States/DispatchHookState.cs
@@ -1,14 +1,22 @@
 using Character.Utils;
+using UnityEngine;
 
 namespace Character.States
 {
     public class DispatchHookState : ACharacterState
     {
+        private const float RaycastDistance = 2f;
+
         public DispatchHookState(CharacterEntity characterEntity) : base(characterEntity) {}
 
         public override void Enter()
         {
-            CharacterEntity.GrapplingHookState.SetHookDispatchState();
+            var origin = new Vector3(Transform.position.x, 1f, Transform.position.z);
+            var direction = Transform.forward;
+
+            Physics.Raycast(origin, direction, out var hit, RaycastDistance);
+            if (hit.collider) CharacterEntity.CharacterState.SetWalkState();
+            else CharacterEntity.GrapplingHookState.SetHookDispatchState();
         }
     }
 }
