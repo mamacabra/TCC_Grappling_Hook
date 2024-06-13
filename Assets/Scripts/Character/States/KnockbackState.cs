dca519d5f2ef319a40a91c162f3bfa3b6a377dc5
guifxbr <guinogfxbr@gmail.com>
2024-06-13 13:05:26 -0300

    knockback cleaning

--- Assets/Scripts/Character/States/KnockbackState.cs
+++ Assets/Scripts/Character/States/KnockbackState.cs
@@ -5,10 +5,10 @@ namespace Character.States
 {
     public class KnockbackState : ACharacterState
     {
-        private float knockbackForce = 60.0f;
-        private float upwardForce = 45f;
+        private float knockbackForce = 100.0f;
+        
         private Vector3 knockbackDirection;
-        private float knockbackDuration = 0.3f;
+        private float knockbackDuration = 0.2f;
         private float knockbackTimer;
         private bool hasHitBack;
 
@@ -28,10 +28,11 @@ namespace Character.States
             if (knockbackTimer > 0)
             {
                 float knockbackStep = knockbackForce * Time.deltaTime;
-                float upwardStep = upwardForce * Time.deltaTime;
+                
 
-                Vector3 knockbackVector = (-knockbackDirection * knockbackStep)/* + (Vector3.up * upwardStep)*/;
+                Vector3 knockbackVector = (-knockbackDirection * knockbackStep);
                 Vector3 newPosition = CharacterEntity.Rigidbody.position + knockbackVector;
+
                 var rayBackDirection = (-Transform.forward).normalized;
                 var origin = new Vector3(Transform.position.x, 1f, Transform.position.z);
                 Physics.Raycast(origin, rayBackDirection, out var hitBack, RaycastDistance);
