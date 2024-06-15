31fb0b0b925ad9f7840fbfefa2adfdc55a9329ed
Jonathan Bergson <contato@bergson.me>
2024-06-15 19:05:16 -0300

    feat(Animations): Implementa VFX de 'dash' para o personagem
    Foi adicionado um novo estado de 'dash' para a controller de animações do personagem 'Sushi'. No script 'DashState.cs', também foram feitas alterações para a ativação e desativação da animação 'dash' quando necessária. Além disso, eventos de partículas referentes ao 'dash' foram adicionados.

--- Assets/Scripts/Character/States/DashState.cs
+++ Assets/Scripts/Character/States/DashState.cs
@@ -8,13 +8,17 @@ namespace Character.States
         private float countDown;
         private const float DashSpeed = 70f;
         private const float DashDuration = 0.12f;
+        private ParticleSystem dashVfx;
 
         public DashState(CharacterEntity characterEntity) : base(characterEntity) {}
 
         public override void Enter()
         {
             CharacterEntity.Character.UseDash();
-            CharacterEntity.CharacterMesh.animator?.SetFloat("Speed", 1);
+            CharacterEntity.CharacterMesh.animator?.SetFloat("Speed", 0);
+            CharacterEntity.CharacterMesh.animator?.SetBool("isDash", true);
+            dashVfx = CharacterEntity.CharacterMesh.animator?.transform.Find("vfx.Dash").GetComponent<ParticleSystem>();
+            dashVfx?.Play();
         }
 
         public override void FixedUpdate()
@@ -26,6 +30,12 @@ namespace Character.States
             {
                 CharacterEntity.CharacterState.SetWalkState();
             }
+        }
+
+        public override void Exit()
+        {
+            CharacterEntity.CharacterMesh.animator?.SetBool("isDash", false);
+            dashVfx?.Stop();
         }
     }
 }
