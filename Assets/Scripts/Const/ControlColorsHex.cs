7b50439fc468d1b5012134b8686a0a5fff2e8275
Jonathan Bergson <contato@bergson.me>
2024-06-23 18:49:55 -0300

    refactor(ControlColors): Atualiza cores de controle hexadecimal

--- Assets/Scripts/Const/ControlColorsHex.cs
+++ Assets/Scripts/Const/ControlColorsHex.cs
@@ -4,12 +4,12 @@ namespace Const
 {
     public static class ControlColorsHex
     {
-        public static Color Yellow => GetColor("#f9ca24");
-        public static Color Green => GetColor("#6ab04c");
-        public static Color Blue => GetColor("#4834d4");
-        public static Color Purple => GetColor("#be2edd");
-        public static Color Red => GetColor("#eb4d4b");
-        public static Color White => GetColor("#22a6b3");
+        public static Color Yellow => GetColor("#fed330");  // rgba(254, 211, 48, 1)
+        public static Color Green => GetColor("#26de81");   // rgba(38, 222, 129, 1)
+        public static Color Blue => GetColor("#45aaf2");    // rgba(69, 170, 242, 1)
+        public static Color Purple => GetColor("#a55eea");  // rgba(165, 94, 234, 1)
+        public static Color Red => GetColor("#eb3b5a");     // rgba(235, 59, 90, 1)
+        public static Color White => GetColor("#dff9fb");   // rgba(223, 249, 251, 1)
 
         private static Color GetColor(string hex)
         {
