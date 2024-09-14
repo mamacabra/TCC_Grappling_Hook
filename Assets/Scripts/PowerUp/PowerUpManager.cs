2878b01b656196399b72c1c90281604430671a81
Jonathan Bergson <contato@bergson.me>
2024-09-14 18:03:15 -0300

    feat: criado lógica para spawnar power ups

--- Assets/Scripts/PowerUp/PowerUpManager.cs
+++ Assets/Scripts/PowerUp/PowerUpManager.cs
@@ -1,12 +1,25 @@
 using System.Collections.Generic;
 using Const;
+using UnityEngine;
 using Enumerable = System.Linq.Enumerable;
 using Random = UnityEngine.Random;
 
 namespace PowerUp
 {
-    public static class PowerUpManager
+    public class PowerUpManager : MonoBehaviour
     {
+        public static PowerUpManager Instance;
+
+        [Header("Power Up")]
+        [SerializeField] private GameObject powerUpItem;
+
+        [Header("Spawn")]
+        [SerializeField] private bool isSpawnEnable;
+        [SerializeField] private GameObject[] spawnPoints;
+
+        [SerializeField] private float spawnCountDown;
+        [SerializeField] private float spawnCountDownDefault = 0.5f;
+
         private static readonly PowerUpVariants[] AvailablePowerUps =
         {
             PowerUpVariants.CharacterShieldPowerUp,
@@ -18,6 +31,66 @@ namespace PowerUp
             // PowerUpVariants.HookTriplePowerUp,
             // PowerUpVariants.HookUntouchablePowerUp,
         };
+
+        public void Awake()
+        {
+            if (Instance != null) Destroy(gameObject);
+            else Instance = this;
+        }
+
+        public void FixedUpdate()
+        {
+            if (isSpawnEnable == false) return;
+
+            spawnCountDown -= Time.fixedDeltaTime;
+            if (spawnCountDown <= 0)
+                SpawnPowerUp();
+        }
+
+        private void SpawnPowerUp()
+        {
+            ResetSpawnCountDown();
+            if (spawnPoints.Length <= 0) return;
+
+            var index = Random.Range(0, spawnPoints.Length);
+            var spawnTransform = spawnPoints[index].gameObject.transform;
+            Instantiate(powerUpItem, spawnTransform);
+        }
+
+        public void StartSpawn()
+        {
+            if (isSpawnEnable) return;
+
+            isSpawnEnable = true;
+            FindAndDestroyPowerUps();
+            FindPowerUpsSpawnPoints();
+            ResetSpawnCountDown();
+        }
+
+        public void StopSpawn()
+        {
+            isSpawnEnable = false;
+            ResetSpawnCountDown();
+        }
+
+        private void ResetSpawnCountDown()
+        {
+            spawnCountDown = spawnCountDownDefault;
+        }
+
+        private static void FindAndDestroyPowerUps()
+        {
+            var powerUps = GameObject.FindGameObjectsWithTag(Tags.PowerUp);
+            foreach (var powerUp in powerUps)
+                Destroy(powerUp);
+        }
+
+        private void FindPowerUpsSpawnPoints()
+        {
+            var a = GameObject.FindGameObjectsWithTag(Tags.PowerUpSpawner);
+            spawnPoints = a;
+            if (spawnPoints.Length == 0) StopSpawn();
+        }
 
         public static PowerUpVariants? Catch(List<PowerUpVariants> characterPowerUps)
         {
