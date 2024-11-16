28dedc9bb2615bc4dbee13d3ee6bdfbb2a8bf654
Jonathan Bergson <contato@bergson.me>
2024-11-16 12:25:40 -0300

    feat: criado chamada para os novos VFX

--- Assets/Scripts/VFX/VFXManager.cs
+++ Assets/Scripts/VFX/VFXManager.cs
@@ -6,7 +6,10 @@ namespace VFX
     {
         public static VFXManager Instance;
 
+        [SerializeField] private ParticleSystem hookDispatchVfx;
+        [SerializeField] private ParticleSystem hookHitVfx;
         [SerializeField] private ParticleSystem parryVfx;
+        [SerializeField] private ParticleSystem victoryVfx;
 
         public void Awake()
         {
@@ -14,18 +17,41 @@ namespace VFX
             else Instance = this;
         }
 
+        public void PlayHookDispatchVFX(Vector3 position)
+        {
+            if (hookDispatchVfx is null) return;
+            Instantiate(hookDispatchVfx, position, Quaternion.identity);
+            hookDispatchVfx.Play();
+        }
+
+        public void PlayHookHitVFX(Vector3 position)
+        {
+            if (hookHitVfx is null) return;
+            Instantiate(hookHitVfx, position, Quaternion.identity);
+            hookHitVfx.Play();
+        }
+
         public void PlayParryVFX(Vector3 position)
         {
+            if (parryVfx is null) return;
             Instantiate(parryVfx, position, Quaternion.identity);
             parryVfx.Play();
         }
 
         public void PlayParryVFX(Vector3 position, Vector3 enemyPosition)
         {
+            if (parryVfx is null) return;
             var middlePoint = (position + enemyPosition) / 2;
 
             Instantiate(parryVfx, middlePoint, Quaternion.identity);
             parryVfx.Play();
+        }
+
+        public void PlayVictoryVFX(Vector3 position)
+        {
+            if (victoryVfx is null) return;
+            Instantiate(victoryVfx, position, Quaternion.identity);
+            victoryVfx.Play();
         }
     }
 }
